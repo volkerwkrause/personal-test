b8f7aa05bfbd21a326ab555f5c90d6bc0e44ac6b
Volker Krause <vkrause@ubin-ag.de>
2011-04-11 14:05:39 +0200

    Initial Commit

---
+++ GitTest/GitTest/GitTest.cpp
@@ -0,0 +1,12 @@
+// GitTest.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	_putts(_T("hello world"));
+	return 0;
+}
+
